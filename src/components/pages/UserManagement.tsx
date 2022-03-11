0cad584e91a20903eb0ddee77a7e1d0a75cbab40
ryota-09 <n.ryo950911@gmail.com>
2022-03-11 15:20:17 +0900

    モーダル作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,16 +1,37 @@
-import { FC, memo, useEffect } from "react";
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { FC, memo, useCallback, useEffect } from "react";
+import {
+  Center,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  Stack,
+  useDisclosure,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 
 import { UserCard } from "../../components/pages/organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 //memoは全体を囲う。
 export const UserManagement: FC = memo(() => {
   const { getUsers, loading, userList } = useAllUsers();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   //画面表示時の処理(created(){})
   useEffect(() => {
     getUsers();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onClickUser = useCallback(() => {
+    onOpen();
   }, []);
 
   return (
@@ -20,18 +41,51 @@ export const UserManagement: FC = memo(() => {
           <Spinner />
         </Center>
       ) : (
-        <Wrap p={{ base: 4, md: 10 }} >
+        <Wrap p={{ base: 4, md: 10 }}>
           {userList.map((user) => (
             <WrapItem key={user.id} mx="auto">
               <UserCard
                 imageUrl="https://source.unsplash.com/random"
                 userName={user.username}
                 fullName={user.name}
+                onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        autoFocus={false}
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent pb={6}>
+          <ModalHeader>ユーザー詳細</ModalHeader>
+          <ModalBody mx={4}>
+            <Stack spacing={4}>
+              <FormControl>
+                <FormLabel>名前</FormLabel>
+                <Input value="鈴木" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>フルネーム</FormLabel>
+                <Input value="鈴木" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>mail</FormLabel>
+                <Input value="鈴木" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Tel</FormLabel>
+                <Input value="鈴木" isReadOnly />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+          <ModalCloseButton />
+        </ModalContent>
+      </Modal>
     </>
   );
 });
--- src/components/pages/organisms/user/UserCard.tsx
+++ src/components/pages/organisms/user/UserCard.tsx
@@ -1,15 +1,16 @@
 import { Box, Image, Stack, Text } from "@chakra-ui/react";
-import { FC, memo, ReactNode } from "react";
+import { FC, memo } from "react";
 
 type Props = {
   imageUrl: string;
   userName: string;
   fullName: string;
+  onClick: () => void;
 };
 
 //memoは全体を囲う。
 export const UserCard: FC<Props> = memo((props) => {
-  const { imageUrl, userName, fullName } = props;
+  const { imageUrl, userName, fullName, onClick } = props;
   return (
     <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" p={4} _hover={{ cursor: "pointer", opacity: 0.8 }}>
     <Stack textAlign="center">
@@ -19,6 +20,7 @@ export const UserCard: FC<Props> = memo((props) => {
         alt={userName}
         m="auto"
         src={imageUrl}
+        onClick={onClick}
       />
       <Text fontSize="lg" fontWeight="bold" >{userName}</Text>
       <Text fontSize="sm" color="gray">{fullName}</Text>
